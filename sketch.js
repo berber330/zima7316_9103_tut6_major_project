3227b834c33c508cd89510f6f393f6f707368503
berber330 <zima7316@uni.sydney.edu.au>
2023-11-06 17:31:34 +1100

    Version 3

--- sketch.js
+++ sketch.js
@@ -14,12 +14,13 @@ let fft;
 let aa = 255;
 let isInit = false;
 let offset = 0;
+let speed = 0
 let playButton;
 
 // Set a fixed class for rectangles of different colours and sizes
 // This distinction can facilitate subsequent animations and endow them with unique functions
 class yellowRect{
-  constructor(x,y,width,height,rotation){
+  constructor(x, y, width, height, rotation){
     this.x = x ?? 0;
     this.y = y ?? 0;
     this.width = width ?? canvasWidth;
@@ -33,28 +34,31 @@ class yellowRect{
     rotate(this.rotation);
 
     noStroke();
-    fill(250, 201, 1, aa);// Add transparency
-    rect(0,0,this.width,this.height);
+    fill(250, 201, 1);
+    rect(0, 0, this.width, this.height);
 
     pop();
   }
 }
 
 class blueRect{
-  constructor(x,y,width,height,rotation){
+  constructor(x, y, width, height, rotation, move){
     this.x = x ?? 0;
     this.y = y ?? 0;
     this.width = width ?? canvasWidth;
     this.height = height ?? canvasHeight;
     this.rotation = rotation ?? 0;
-// Select a random jitter direction for the rectangle
+// Select a random shake direction for the rectangle
     this.d = int(random(4)) + 1;
+    this.dd = int(random(4)) + 1;
+// Indicates whether the rectangle will move
+    this.move = move; 
   }
 
   draw(){
     push();
 
-// Jitter by direction
+// Shake by direction
     if (this.d == 1) {
       translate(this.x + offset, this.y);
     } else if (this.d == 2) {
@@ -69,26 +73,71 @@ class blueRect{
 
     noStroke();
     fill(34, 80, 149, aa);// Add transparency
-    rect(0,0,this.width,this.height);
+    rect(0, 0, this.width, this.height);
 
     pop();
+
+    if (this.move) {
+      if (checkNext(this.dd, { x: this.x, y: this.y }, speed)) {
+// Move according to direction
+        if (this.dd == 1) {
+          // up
+          this.y = this.y - speed;
+        } else if (this.dd == 2) {
+          // right
+          this.x = this.x + speed;
+        } else if (this.dd == 3) {
+          // down
+          this.y = this.y + speed;
+        } else if (this.dd == 4) {
+          // left
+          this.x = this.x - speed;
+        }
+      } else {
+        this.dd = int(random(4)) + 1;
+      }
+    }
+  }
+}
+
+// Determine if the next position is inside the yellow rectangle
+function checkNext(dd, pos, speed) {
+  if (dd == 1) {
+    // up
+    let col = get(pos.x, pos.y - speed);
+    return col[0] === 250 && col[1] === 201 && col[2] === 1;
+  } else if (dd == 2) {
+    // right
+    let col = get(pos.x + pixelLength + speed, pos.y);
+    return col[0] === 250 && col[1] === 201 && col[2] === 1;
+  } else if (dd == 3) {
+    // down
+    let col = get(pos.x, pos.y + pixelLength + speed);
+    return col[0] === 250 && col[1] === 201 && col[2] === 1;
+  } else if (dd == 4) {
+    // left
+    let col = get(pos.x - speed, pos.y);
+    return col[0] === 250 && col[1] === 201 && col[2] === 1;
   }
 }
 
 class redRect{
-  constructor(x,y,width,height,rotation){
+  constructor(x, y, width, height, rotation, move){
     this.x = x ?? 0;
     this.y = y ?? 0;
     this.width = width ?? canvasWidth;
     this.height = height ?? canvasHeight;
     this.rotation = rotation ?? 0;
-// Select a random jitter direction for the rectangle
+// Select a random shake direction for the rectangle
     this.d = int(random(4)) + 1;
+    this.dd = int(random(4)) + 1;
+// Indicates whether the rectangle will move
+    this.move = move;
   }
 
   draw(){
     push();
-// Jitter by direction
+// Shake by direction
      if (this.d == 1) {
       translate(this.x + offset, this.y);
     } else if (this.d == 2) {
@@ -102,26 +151,50 @@ class redRect{
 
     noStroke();
     fill(221, 1, 0, aa);// Add transparency
-    rect(0,0,this.width,this.height);
+    rect(0, 0, this.width, this.height);
 
     pop();
+
+    if (this.move) {
+      if (checkNext(this.dd, { x: this.x, y: this.y }, speed)) {
+// Move according to direction
+        if (this.dd == 1) {
+          // up
+          this.y = this.y - speed;
+        } else if (this.dd == 2) {
+          // right
+          this.x = this.x + speed;
+        } else if (this.dd == 3) {
+          // down
+          this.y = this.y + speed;
+        } else if (this.dd == 4) {
+          // left
+          this.x = this.x - speed;
+        }
+      } else {
+        this.dd = int(random(4)) + 1;
+      }
+    }
   }
 }
 
 class grayRect{
-  constructor(x,y,width,height,rotation){
+  constructor(x, y, width, height, rotation, move){
     this.x = x ?? 0;
     this.y = y ?? 0;
     this.width = width ?? canvasWidth;
     this.height = height ?? canvasHeight;
     this.rotation = rotation ?? 0;
-// Select a random jitter direction for the rectangle
+// Select a random shake direction for the rectangle
     this.d = int(random(4)) + 1;
-    }
+    this.dd = int(random(4)) + 1;
+// Indicates whether the rectangle will move
+    this.move = move;
+  }
 
   draw(){
     push();
-// Jitter by direction
+// Shake by direction
     if (this.d == 1) {
       translate(this.x + offset, this.y);
     } else if (this.d == 2) {
@@ -135,9 +208,30 @@ class grayRect{
 
     noStroke();
     fill(200, aa);// Add transparency
-    rect(0,0,this.width,this.height);
+    rect(0, 0, this.width, this.height);
 
     pop();
+
+    if (this.move) {
+      if (checkNext(this.dd, { x: this.x, y: this.y }, speed)) {
+// Move according to direction
+        if (this.dd == 1) {
+          // up
+          this.y = this.y - speed;
+        } else if (this.dd == 2) {
+          // right
+          this.x = this.x + speed;
+        } else if (this.dd == 3) {
+          // down
+          this.y = this.y + speed;
+        } else if (this.dd == 4) {
+          // left
+          this.x = this.x - speed;
+        }
+      } else {
+        this.dd = int(random(4)) + 1;
+      }
+    }
   }
 }
 
@@ -150,71 +244,71 @@ function preload() {
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
 
-  yellowRects.push(new yellowRect(0,20,1000,pixelLength));
-  yellowRects.push(new yellowRect(0,140,1000,pixelLength));
-  yellowRects.push(new yellowRect(0,320,1000,pixelLength));
-  yellowRects.push(new yellowRect(0,380,1000,pixelLength));
-  yellowRects.push(new yellowRect(0,500,1000,pixelLength));
-  yellowRects.push(new yellowRect(0,560,1000,pixelLength));
-  yellowRects.push(new yellowRect(0,620,60,pixelLength));
-  yellowRects.push(new yellowRect(60,660,460,pixelLength));
-  yellowRects.push(new yellowRect(0,700,60,pixelLength));
-  yellowRects.push(new yellowRect(0,760,1000,pixelLength));
-  yellowRects.push(new yellowRect(0,800,60,pixelLength));
-  yellowRects.push(new yellowRect(0,860,1000,pixelLength));
-  yellowRects.push(new yellowRect(0,960,1000,pixelLength));
+  yellowRects.push(new yellowRect(0, 20, 1000, pixelLength));
+  yellowRects.push(new yellowRect(0, 140, 1000, pixelLength));
+  yellowRects.push(new yellowRect(0, 320, 1000, pixelLength));
+  yellowRects.push(new yellowRect(0, 380, 1000, pixelLength));
+  yellowRects.push(new yellowRect(0, 500, 1000, pixelLength));
+  yellowRects.push(new yellowRect(0, 560, 1000, pixelLength));
+  yellowRects.push(new yellowRect(0, 620, 60, pixelLength));
+  yellowRects.push(new yellowRect(60, 660, 460, pixelLength));
+  yellowRects.push(new yellowRect(0, 700, 60, pixelLength));
+  yellowRects.push(new yellowRect(0, 760, 1000, pixelLength));
+  yellowRects.push(new yellowRect(0, 800, 60, pixelLength));
+  yellowRects.push(new yellowRect(0, 860, 1000, pixelLength));
+  yellowRects.push(new yellowRect(0, 960, 1000, pixelLength));
 
-  yellowRects.push(new yellowRect(20,0,pixelLength,320));
-  yellowRects.push(new yellowRect(60,0,pixelLength,1000));
-  yellowRects.push(new yellowRect(120,0,pixelLength,860));
-  yellowRects.push(new yellowRect(240,0,pixelLength,1000));
-  yellowRects.push(new yellowRect(480,0,pixelLength,1000));
-  yellowRects.push(new yellowRect(520,0,pixelLength,320));
-  yellowRects.push(new yellowRect(520,380,pixelLength,620));
-  yellowRects.push(new yellowRect(600,380,pixelLength,180));
-  yellowRects.push(new yellowRect(800,0,pixelLength,1000));
-  yellowRects.push(new yellowRect(860,0,pixelLength,320));
-  yellowRects.push(new yellowRect(900,0,pixelLength,380));
-  yellowRects.push(new yellowRect(900,560,pixelLength,220));
-  yellowRects.push(new yellowRect(960,0,pixelLength,1000));
+  yellowRects.push(new yellowRect(20, 0, pixelLength, 320));
+  yellowRects.push(new yellowRect(60, 0, pixelLength, 1000));
+  yellowRects.push(new yellowRect(120, 0, pixelLength, 860));
+  yellowRects.push(new yellowRect(240, 0, pixelLength, 1000));
+  yellowRects.push(new yellowRect(480, 0, pixelLength, 1000));
+  yellowRects.push(new yellowRect(520, 0, pixelLength, 320));
+  yellowRects.push(new yellowRect(520, 380, pixelLength, 620));
+  yellowRects.push(new yellowRect(600, 380, pixelLength, 180));
+  yellowRects.push(new yellowRect(800, 0, pixelLength, 1000));
+  yellowRects.push(new yellowRect(860, 0, pixelLength, 320));
+  yellowRects.push(new yellowRect(900, 0, pixelLength, 380));
+  yellowRects.push(new yellowRect(900, 560, pixelLength, 220));
+  yellowRects.push(new yellowRect(960, 0, pixelLength, 1000));
 
-  extraYellowRects.push(new yellowRect(120,60,120,pixelLength*2));
-  extraYellowRects.push(new yellowRect(120,240,120,pixelLength*3));
-  extraYellowRects.push(new yellowRect(800,420,160,pixelLength*3));
-  extraYellowRects.push(new yellowRect(800,660,160,pixelLength*2));
-  extraYellowRects.push(new yellowRect(120,700,120,pixelLength*3));
-  extraYellowRects.push(new yellowRect(160,320,pixelLength*3,80));
-  extraYellowRects.push(new yellowRect(300,380,pixelLength*3,120));
-  extraYellowRects.push(new yellowRect(400,320,pixelLength*3,200));
+  extraYellowRects.push(new yellowRect(120, 60, 120, pixelLength*2));
+  extraYellowRects.push(new yellowRect(120, 240, 120, pixelLength*3));
+  extraYellowRects.push(new yellowRect(800, 420, 160, pixelLength*3));
+  extraYellowRects.push(new yellowRect(800, 660, 160, pixelLength*2));
+  extraYellowRects.push(new yellowRect(120, 700, 120, pixelLength*3));
+  extraYellowRects.push(new yellowRect(160, 320, pixelLength*3, 80));
+  extraYellowRects.push(new yellowRect(300, 380, pixelLength*3, 120));
+  extraYellowRects.push(new yellowRect(400, 320, pixelLength*3, 200));
 
-  blueRects.push(new blueRect(80,180,pixelLength*3,60));
-  blueRects.push(new blueRect(80,600,pixelLength*3,60));
-  blueRects.push(new blueRect(300,420,pixelLength*3,80));
-  blueRects.push(new blueRect(600,160,pixelLength*5,160));
-  blueRects.push(new blueRect(880,100,pixelLength*4,40));
-  blueRects.push(new blueRect(820,600,pixelLength*4,60));
+  blueRects.push(new blueRect(80, 180, pixelLength*3, 60));
+  blueRects.push(new blueRect(80, 600, pixelLength*3, 60));
+  blueRects.push(new blueRect(300, 420, pixelLength*3, 80));
+  blueRects.push(new blueRect(600, 160, pixelLength*5, 160));
+  blueRects.push(new blueRect(880, 100, pixelLength*4, 40));
+  blueRects.push(new blueRect(820, 600, pixelLength*4, 60));
 
-  redRects.push(new redRect(160,40,pixelLength*3,100));
-  redRects.push(new redRect(140,440,pixelLength*5,60));
-  redRects.push(new redRect(280,40,pixelLength*4,100));
-  redRects.push(new redRect(400,860,pixelLength*3,140));
-  redRects.push(new redRect(600,200,pixelLength*5,80));
-  redRects.push(new redRect(860,180,pixelLength*3,60));
-  redRects.push(new redRect(640,400,pixelLength*5,160));
-  redRects.push(new redRect(880,420,pixelLength,60));
-  redRects.push(new redRect(820,700,pixelLength*4,60));
+  redRects.push(new redRect(160, 40, pixelLength*3, 100));
+  redRects.push(new redRect(140, 440, pixelLength*5, 60));
+  redRects.push(new redRect(280, 40, pixelLength*4, 100));
+  redRects.push(new redRect(400, 860, pixelLength*3, 140));
+  redRects.push(new redRect(600, 200, pixelLength*5, 80));
+  redRects.push(new redRect(860, 180, pixelLength*3, 60));
+  redRects.push(new redRect(640, 400, pixelLength*5, 160));
+  redRects.push(new redRect(880, 420, pixelLength, 60));
+  redRects.push(new redRect(820, 700, pixelLength*4, 60));
 
-  grayRects.push(new grayRect(160,100,pixelLength*3,20));
-  grayRects.push(new grayRect(160,260,pixelLength*3,20));
-  grayRects.push(new grayRect(180,340,pixelLength,40));
-  grayRects.push(new grayRect(180,720,pixelLength*2,20));
-  grayRects.push(new grayRect(300,60,pixelLength*2,40));
-  grayRects.push(new grayRect(280,120,pixelLength*4,20));
-  grayRects.push(new grayRect(400,380,pixelLength*3,20));
-  grayRects.push(new grayRect(400,420,pixelLength*3,60));
-  grayRects.push(new grayRect(400,900,pixelLength*3,60));
-  grayRects.push(new grayRect(660,420,pixelLength*3,60));
-  grayRects.push(new grayRect(640,540,pixelLength*5,20));
+  grayRects.push(new grayRect(160, 100, pixelLength*3, 20));
+  grayRects.push(new grayRect(160, 260, pixelLength*3, 20));
+  grayRects.push(new grayRect(180, 340, pixelLength, 40));
+  grayRects.push(new grayRect(180, 720, pixelLength*2, 20));
+  grayRects.push(new grayRect(300, 60, pixelLength*2, 40));
+  grayRects.push(new grayRect(280, 120, pixelLength*4, 20));
+  grayRects.push(new grayRect(400, 380, pixelLength*3, 20));
+  grayRects.push(new grayRect(400, 420, pixelLength*3, 60));
+  grayRects.push(new grayRect(400, 900, pixelLength*3, 60));
+  grayRects.push(new grayRect(660, 420, pixelLength*3, 60));
+  grayRects.push(new grayRect(640, 540, pixelLength*5, 20));
 
 // Create FFT for audio visualization
   fft = new p5.FFT(0.3);
@@ -238,12 +332,16 @@ function draw() {
   fft.analyze();
 // Obtain audio data in the frequency range of 20 to 200 Hz (bass part)
   amp = fft.getEnergy(20, 200); 
-// Convert the audio data from '0-200' to '0-255'
-  aa = map(amp, 0, 200, 0, 255); 
+// Convert the audio data from '100-200' to '0-255'
+  aa = map(amp, 100, 200, 0, 255); 
 // Obtain audio data in the frequency range of 200 to 2000 Hz (from mid-low to mid-high range part)
   amp = fft.getEnergy(200, 2000); 
-// Convert the audio data from '100-150' to '0-20'
-  offset = map(amp, 100, 150, 0, 20); 
+// Convert the audio data from '100-150' to '0-10'
+  offset = map(amp, 100, 150, 0, 10); 
+// Obtain audio data in the frequency range of 2000 to 3000 Hz (treble part)
+  amp = fft.getEnergy(2000, 3000); 
+// Convert the audio data from '60-150' to '0.5-2'
+  speed = map(amp, 60, 150, 0.5, 2);
   }
 
   yellowRects.forEach(r => r.draw());
@@ -289,13 +387,14 @@ function generateRandomRectangles() {
 // Create corresponding rectangles based on colour
     if (colorIndex == 0) {
       grayRects.push(
-        new grayRect(region.x, region.y, pixelLength, pixelLength)
+        new grayRect(region.x, region.y, pixelLength, pixelLength, 0, true)
       );
     } else if (colorIndex == 1) {
-      redRects.push(new redRect(region.x, region.y, pixelLength, pixelLength));
+      redRects.push(new redRect(region.x, region.y, pixelLength, pixelLength, 0, true)
+      );
     } else if (colorIndex == 2) {
       blueRects.push(
-        new blueRect(region.x, region.y, pixelLength, pixelLength)
+        new blueRect(region.x, region.y, pixelLength, pixelLength, 0, true)
       );
     }
   }
